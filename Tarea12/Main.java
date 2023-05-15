0eddfbb1af2cbd9624768331e89f97179a845b4c
KevinJimenezS <125950028+KevinJimenezS@users.noreply.github.com>
2023-05-14 20:55:37 -0600

    Create Main.java

---
+++ Tarea12/Main.java
@@ -0,0 +1,9 @@
+package Tarea12;
+
+import Tarea11.Ventana2;
+
+public class Main {
+    public static void main(String[] args) {
+        VentanaV ventanaV = new VentanaV("EJEMPLO TABLA MVC");
+    }
+}
